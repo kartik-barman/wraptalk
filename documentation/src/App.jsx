3c40b15fa2b03f2db99fc8880f69a77734349685
Yousuf-cse <yousuf.mallik79@gmail.com>
2025-03-29 14:27:19 +0530

    initialize and set react router for the project

--- documentation/src/App.jsx
+++ documentation/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-     <h1 className='text-green-500'>Hello world</h1>
+    
     </>
   )
 }
--- documentation/src/main.jsx
+++ documentation/src/main.jsx
@@ -1,11 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Layout from './Layout.jsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import LandingPage from './Components/LandingPage.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<LandingPage/>} />
+      
+      
+      
+      
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <LandingPage />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
