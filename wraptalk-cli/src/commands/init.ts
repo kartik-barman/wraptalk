c09d2a404a33cfa622aef064f6a6607966cc0d87
sreegopalsaha <sreegopal0101@gmail.com>
2025-04-01 14:42:46 +0530

    update init command to reflect package name change and improve console messages

--- wraptalk-cli/src/commands/init.ts
+++ wraptalk-cli/src/commands/init.ts
@@ -12,10 +12,10 @@ function detectProjectType(): string[] {
 }
 
 export function initCommand() {
-  console.log("Installing wraptalk-reactjs...");
+  console.log("Installing wraptalk-react...");
 
   try {
-    execSync("npm install wraptalk-reactjs", { stdio: "inherit" });
+    execSync("npm install wraptalk-react", { stdio: "inherit" });
   } catch (error) {
     console.error("Failed to install wraptalk-react.");
     console.error(error);
@@ -30,7 +30,7 @@ export function initCommand() {
     process.exit(1);
   }
 
-  console.log("Found src directory. Creating config files...");
+  // console.log("Found src directory. Creating config files...");
 
   try {
     const translationsPath = path.join(srcPath, "wraptalk.translations.json");
@@ -43,7 +43,7 @@ export function initCommand() {
     fs.writeFileSync(configPath, JSON.stringify(configData, null, 2), "utf-8");
     console.log("Created wraptalk.config.json in the root folder");
 
-    console.log("Initialization complete! You can now run `npx wraptalk run`.");
+    console.log("Initialization complete!.");
   } catch (error) {
     console.error("Error creating configuration files:", error);
     process.exit(1);
